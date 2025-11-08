5e5fd2cf43851931c1d82e8c9dd6edfd523083a0
PomPerWoW <chatthanes22@gmail.com>
2025-11-09 04:43:39 +0700

    Add allowedHosts config in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["turing.se.kmitl.ac.th"],
     port: 5173,
     host: true,
   },
-})
+});
