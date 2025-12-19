b01fd61216201696dbd767ad3efeaf48fcab37eb
PomPerWoW <chatthanes22@gmail.com>
2025-12-20 02:46:24 +0700

    Handle env files

--- apps/scene-creator/src/api/axios.ts
+++ apps/scene-creator/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { config } from "../config/env";
 
 const api = axios.create({
-  baseURL: config.API_URL,
+  baseURL: config.BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },
