88c3111cb0af8072e4ae44a7d2476d40e80b15e4
PomPerWoW <chatthanes22@gmail.com>
2026-01-12 21:17:29 +0700

    Add on/off to device model

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import { devtools } from "@tanstack/devtools-vite";
+// import { devtools } from "@tanstack/devtools-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    devtools(),
+    // devtools(),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
