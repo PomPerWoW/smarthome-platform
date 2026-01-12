53a2bdbf3c2a26b7ad81d848acde2e4d3e90292a
Inthat <Inthat.sapp@gmail.com>
2026-01-12 17:39:08 +0700

    add new files in barrel file for exports

--- apps/frontend/src/stores/index.ts
+++ apps/frontend/src/stores/index.ts
@@ -1,7 +1,9 @@
-import { useSettingsStore } from './settings';
-import { useUIStore } from './ui';
+import { useAuthStore } from './auth';
+import { useSettingsStore } from './settings_store';
+import { useUIStore } from './ui_store';
 
 export {
+  useAuthStore,
   useSettingsStore,
   useUIStore,
 };
